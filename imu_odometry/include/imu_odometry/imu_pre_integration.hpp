bdf84189979ebd863f689357b62472113532ba1f
Zhenpeng Ge <zhenpeng.ge@qq.com>
2023-07-05 23:23:00 +0800

    add imu_odometry (#20)

---
+++ imu_odometry/include/imu_odometry/imu_pre_integration.hpp
@@ -0,0 +1,96 @@
+// Copyright 2023 Gezp (https://github.com/gezp).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <deque>
+
+#include "localization_common/sensor_data/imu_data.hpp"
+#include "localization_common/sensor_data/imu_nav_state.hpp"
+
+namespace imu_odometry
+{
+
+class ImuPreIntegration
+{
+  static const int DIM_STATE = 15;
+  static const int DIM_NOISE = 18;
+
+  static const int INDEX_ALPHA = 0;
+  static const int INDEX_THETA = 3;
+  static const int INDEX_BETA = 6;
+  static const int INDEX_B_A = 9;
+  static const int INDEX_B_G = 12;
+
+  static const int INDEX_N_A_PREV = 0;
+  static const int INDEX_N_G_PREV = 3;
+  static const int INDEX_N_A_CURR = 6;
+  static const int INDEX_N_G_CURR = 9;
+  static const int INDEX_N_B_A = 12;
+  static const int INDEX_N_B_G = 15;
+
+public:
+  ImuPreIntegration(
+    double accel_noise, double gyro_noise, double accel_bias_noise, double gyro_bias_noise);
+  void set_bias(Eigen::Vector3d ba, Eigen::Vector3d bg);
+  bool integrate(const localization_common::IMUData & imu_data);
+  bool reset(bool clear_buffer = false);
+  double get_dt();
+  Eigen::Vector3d get_alpha();
+  Eigen::Matrix3d get_theta();
+  Eigen::Vector3d get_beta();
+  Eigen::Matrix<double, 15, 15> get_covariance();
+  Eigen::Matrix<double, 15, 15> get_jacobian();
+  localization_common::ImuNavState get_imu_nav_state(
+    const localization_common::ImuNavState & initial_state);
+
+private:
+  void update_state();
+
+private:
+  // hyper-params:
+  double prior_noise_;
+  double gyro_noise_;
+  double accel_noise_;
+  double gyro_bias_noise_;
+  double accel_bias_noise_;
+  // data buff:
+  std::deque<localization_common::IMUData> imu_data_buff_;
+  double time_;
+  // process noise:
+  Eigen::Matrix<double, DIM_NOISE, DIM_NOISE> Q_;
+  // process equation:
+  Eigen::Matrix<double, DIM_STATE, DIM_STATE> F_;
+  Eigen::Matrix<double, DIM_STATE, DIM_NOISE> B_;
+  // pre-integration state:
+  // time delta:
+  double total_dt_;
+  // relative translation
+  Eigen::Vector3d alpha_ij_;
+  // relative orientation
+  Eigen::Matrix3d theta_ij_;
+  // relative velocity
+  Eigen::Vector3d beta_ij_;
+  // accel bias
+  Eigen::Vector3d ba_i_;
+  // gyro bias
+  Eigen::Vector3d bg_i_;
+  // covariance matrix
+  Eigen::Matrix<double, DIM_STATE, DIM_STATE> P_;
+  // Jacobian for update caused by bias
+  Eigen::Matrix<double, DIM_STATE, DIM_STATE> J_;
+  bool is_inited_{false};
+};
+
+}  // namespace imu_odometry
---
+++ imu_odometry/src/imu_integration.cpp
@@ -0,0 +1,68 @@
+// Copyright 2023 Gezp (https://github.com/gezp).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "imu_odometry/imu_integration.hpp"
+
+#include <sophus/so3.hpp>
+
+namespace imu_odometry
+{
+bool ImuIntegration::reset(const localization_common::ImuNavState & state, bool clear_buffer)
+{
+  state_ = state;
+  if (clear_buffer) {
+    imu_data_buff_.clear();
+    is_inited_ = false;
+  }
+}
+
+bool ImuIntegration::integrate(const localization_common::IMUData & imu_data)
+{
+  if (!is_inited_) {
+    imu_data_buff_.push_back(imu_data);
+    is_inited_ = true;
+    return true;
+  }
+  // check
+  if (imu_data.time <= state_.time) {
+    return false;
+  }
+  imu_data_buff_.push_back(imu_data);
+  // get deltas:
+  double dt = imu_data_buff_.at(1).time - imu_data_buff_.at(0).time;
+  // phi
+  Eigen::Vector3d w0 = imu_data_buff_.at(0).angular_velocity - state_.gyro_bias;
+  Eigen::Vector3d w1 = imu_data_buff_.at(1).angular_velocity - state_.gyro_bias;
+  Eigen::Vector3d phi = 0.5 * (w0 + w1) * dt;
+  // ori
+  Eigen::Matrix3d new_ori = state_.orientation * Sophus::SO3d::exp(phi).matrix();
+  // vel
+  Eigen::Vector3d a0 = imu_data_buff_.at(0).linear_acceleration - state_.accel_bias;
+  Eigen::Vector3d a1 = imu_data_buff_.at(1).linear_acceleration - state_.accel_bias;
+  Eigen::Vector3d a = 0.5 * (state_.orientation * a0 + new_ori * a1);
+  Eigen::Vector3d new_vel = state_.linear_velocity + (a + state_.gravity) * dt;
+  // pos
+  Eigen::Vector3d new_pos = state_.position + 0.5 * (state_.linear_velocity + new_vel) * dt;
+  // update
+  state_.time = imu_data.time;
+  state_.position = new_pos;
+  state_.orientation = new_ori;
+  state_.linear_velocity = new_vel;
+  imu_data_buff_.pop_front();
+  return true;
+}
+
+const localization_common::ImuNavState & ImuIntegration::get_imu_nav_state() {return state_;}
+
+}  // namespace imu_odometry
