a3a99f8e4fe984aafb90106598e80ca960c67563
zhenpeng ge <zhenpeng.ge@qq.com>
2023-10-10 00:09:22 +0800

    add pose data type

---
+++ localization_common/include/localization_common/sensor_data/pose_data.hpp
@@ -0,0 +1,28 @@
+// Copyright 2023 Gezp (https://github.com/gezp).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace localization_common
+{
+
+struct PoseData
+{
+  double time = 0.0;
+  Eigen::Matrix4d pose = Eigen::Matrix4d::Identity();
+};
+
+}  // namespace localization_common
